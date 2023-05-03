231ec79d370f139a1ab1f5a6adb739a9e6dc7a56
Alan Zhao <jiahaozhao1@gmail.com>
2023-05-03 10:00:21 -0500

    5/3/2023

--- download_compute.py
+++ download_compute.py
@@ -2,9 +2,9 @@ from DownloadCSV import download
 from Compute import generateNewestIronConorsEV
 
 def all():
-    # download("BearCall", "https://www.barchart.com/options/call-spreads/bear-call?orderBy=maxProfitPercent&orderDir=desc")
-    # download("BullPut", "https://www.barchart.com/options/put-spreads/bull-put?orderBy=maxProfitPercent&orderDir=desc")
-    # download("IronCon", "https://www.barchart.com/options/short-condors?orderBy=breakEvenProbability&orderDir=desc")
+    download("BearCall", "https://www.barchart.com/options/call-spreads/bear-call?orderBy=maxProfitPercent&orderDir=desc")
+    download("BullPut", "https://www.barchart.com/options/put-spreads/bull-put?orderBy=maxProfitPercent&orderDir=desc")
+    download("IronCon", "https://www.barchart.com/options/short-condors?orderBy=breakEvenProbability&orderDir=desc")
     
     #analyze data 
     generateNewestIronConorsEV("BearCall")
